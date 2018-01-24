4200d6d4445a6f2ba3e5ba1aef4179b99c0d80bb
Carl Wieland <utahwithak@gmail.com>
2018-01-24 12:30:53 -0500

    initial commit

---
+++ Swift Decimator/Swift Decimator/Swift_Decimator.h
@@ -0,0 +1,19 @@
+//
+//  Swift_Decimator.h
+//  Swift Decimator
+//
+//  Created by Carl Wieland on 1/24/18.
+//  Copyright © 2018 Datum Apps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Swift_Decimator.
+FOUNDATION_EXPORT double Swift_DecimatorVersionNumber;
+
+//! Project version string for Swift_Decimator.
+FOUNDATION_EXPORT const unsigned char Swift_DecimatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_Decimator/PublicHeader.h>
+
+
